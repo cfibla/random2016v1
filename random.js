6bc6f720a8f0fa3bd1383cf0fafa886ae6db0612
cfibla <charliesuper@hotmail.com>
2016-01-20 20:18:59 +0100

    'randomV2'

--- random.js
+++ random.js
@@ -1,9 +1,8 @@
 //Math.random() devuelve número aletorio entre 0 y 1
 var numero = Math.random();
+var str = ' MAYOR que 0.5';
 
 if (numero <= 0.5){
-	console.log('\n' + numero + ' es MENOR que 0.5\n');
+	str = ' MENOR que 0.5';
 }
-else {
-	console.log('\n' + numero + ' es MAYOR que 0.5')
-}
+	console.log('\n' + numero + str + '\n')
